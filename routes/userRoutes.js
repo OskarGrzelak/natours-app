6c17827b5574dd06ae0107652ae747db544cf33e
Oskar Grzelak <oskar.grzelak@gmail.com>
2020-12-16 14:18:50 +0100

    Authorization: finished

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,15 +3,30 @@ const {
   getAllUsers,
   createUser,
   getUser,
+  updateMe,
   updateUser,
+  deleteMe,
   deleteUser,
 } = require('../controllers/userController');
-const { signup, login } = require('../controllers/authController');
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
+router.patch('/update-me', protect, updateMe);
+router.delete('/delete-me', protect, deleteMe);
+
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/forgot-password', forgotPassword);
+router.patch('/reset-password/:token', resetPassword);
+router.patch('/update-my-password', protect, updatePassword);
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
